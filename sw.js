1f541f47f0f708f158d1ceaa86771b9dfcd36c00
ELANGESTION <justino.17@hotmail.com>
2026-06-21 22:01:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ELAN GESTION — Service Worker (mode hors-ligne) */
-const CACHE = 'elan-gestion-v116';
+const CACHE = 'elan-gestion-v123';
 const ASSETS = [
   'app.html',
   'manifest.webmanifest',
